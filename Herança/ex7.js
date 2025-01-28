515c59027d545283ad2e2e410739826a43ec754e
MateusIrineu <113316193+MateusIrineu@users.noreply.github.com>
2025-01-28 13:21:38 -0300

    Update ex7.js

--- Herança/ex7.js
+++ Herança/ex7.js
@@ -1,3 +1,28 @@
 /*Crie uma classe base Instrumento com o método tocar. Crie duas classes derivadas
 Violao e Piano, que devem implementar o método tocar exibindo sons específicos.
 */
+
+class Instrumento {
+    tocar() {}
+}
+
+class Violao extends Instrumento {
+    tocar() {
+        console.log("O violão faz brombrombrom.");
+    }
+}
+
+class Piano extends Instrumento {
+    tocar() {
+        console.log("O piano faz plimplimplim.");
+    }
+}
+
+const instrumentos = [
+    new Violao(),
+    new Piano()
+]
+
+instrumentos.forEach((instrumento) => {
+    instrumento.tocar()
+});
